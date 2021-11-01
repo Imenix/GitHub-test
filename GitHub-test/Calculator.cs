aa646301a16b5551150a2631b9f56e26a289f60a
Imenix <89914943+Imenix@users.noreply.github.com>
2021-11-01 14:15:21 +0100

    ad

--- GitHub-test/Calculator.cs
+++ GitHub-test/Calculator.cs
@@ -20,17 +20,29 @@ namespace GitHub_test
             string inputCalc = Console.ReadLine();
             Console.WriteLine("Ange ett andra tal: ");
             int input2 = int.Parse(Console.ReadLine());
+
         }
+
+            
+
+        }
+
+        public int multiply(int input1, int input2)
+        {
+            int answer = input1 * input2;
+                return answer;
+
 
         public int add(int a, int b) // Josefin
         {
             int answer = a + b;
             return answer;
+
         }
-	public int Subtract ( int a, int b)  // Jonas
-	{
-		int result = a- b;
-		return result;
-	}
+	    public int subtract ( int a, int b)  // Jonas
+	    {
+		     int result = a- b;
+		    return result;
+	    }
     }
 }

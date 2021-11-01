b9298167d46f9b3f7204b066352486f3c53e6fd1
Amanda <Amanda@LAPTOP-7QHFK4Q5>
2021-11-01 13:17:56 +0100

    Add project files.

---
+++ GitHub-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHub_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Team!");
+        }
+    }
+}

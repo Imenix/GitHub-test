7687399d54456d15ea244992b0449e7ce97a35f5
Josefin Persson <josefinp94@gmail.com>
2021-11-01 14:01:32 +0100

    plus...

--- GitHub-test/Calculator.cs
+++ GitHub-test/Calculator.cs
@@ -12,7 +12,7 @@ namespace GitHub_test
             
         }
 
-        private void run()
+        public void run()
         {
             Console.WriteLine("Ange ett tal: ");
             int input1 = int.Parse(Console.ReadLine());
@@ -20,7 +20,12 @@ namespace GitHub_test
             string inputCalc = Console.ReadLine();
             Console.WriteLine("Ange ett andra tal: ");
             int input2 = int.Parse(Console.ReadLine());
+        }
 
+        public int add() //Josefin
+        {
+            int answer = input1 + input2;
+            Console.WriteLine(answer);
         }
     }
 }
